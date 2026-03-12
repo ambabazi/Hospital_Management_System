efe721e43ea4203a431ad596e38013a86a4dd883
ambabazi <ambabazi00@gmail.com>
2026-03-12 21:55:00 +0200

    Created a service of saving patients

---
+++ HospitalServices.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.DAO.PatientsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+public class HospitalServices {
+
+    @Autowired
+    private PatientsDAO patientsDAO;
+
+    public void savePatient(Patients patient) throws SQLException {
+        patientsDAO.insertPatients(patient);
+    }
+}

2b038a4c916a14a34f7940abca5c24b744d00e1f
ambabazi <ambabazi00@gmail.com>
2026-03-12 21:52:59 +0200

    Configured the database connection and scanned all beans

---
+++ Config/AppConfig.java
@@ -0,0 +1,17 @@
+package org.example.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+@Configuration
+@ComponentScan("org.example")
+public class AppConfig {
+    @Bean
+    public Connection getConnection() throws SQLException {
+        DatabaseConnection connection = new DatabaseConnection();
+        return connection.getConnection();
+    }
+}
---
+++ Config/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package org.example.Config;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+public class DatabaseConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/HospitalManagement";
+    private static final  String userName = "postgres";
+    private static final  String password = "";
+
+  Connection connection;
+
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url,userName,password);
+    }
+
+
+}
